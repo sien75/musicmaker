f7ecd168dc01bfbb1eebcb4e06bc4592ae66e560
sien75 <qwer0030@126.com>
2018-03-10 23:51:02 +0800

    add color response, and make it more friendly to mobile

--- liveplay/main.js
+++ liveplay/main.js
@@ -5,15 +5,25 @@ var continuousTable = {
   '0390_Aspirin' : true
 }
 
+document.addEventListener('touchmove', function(event) {
+  event.preventDefault();
+}, { passive: false });
+document.addEventListener('contextmenu', function(e){
+    e.preventDefault();
+});
+
 var canvas = document.getElementById('canvas');
 var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
 var audioContext = new AudioContextFunc();
 var player = new WebAudioFontPlayer();
 var loadedInstruments = [];
 var currentInstrument = {};
-var currentPlay;
+var currentPlay = new Array;
+var noteRecord, rectRecord;
 
 var myCanvas = new MyCanvas();
+
+var browser = new Browser();
 
 window.onload = function() {
   myCanvas.init();
@@ -42,15 +52,15 @@ function importScript(url, callback) {
 
 function startNote(note) {
   if (clickOn && note) {
-    currentPlay = player.queueWaveTable(audioContext, audioContext.destination,
+    currentPlay[note] = player.queueWaveTable(audioContext, audioContext.destination,
       window['_tone_' + currentInstrument.tag + '_sf2_file'], 0/*currentTime*/,
       note, currentInstrument.isContinuous?999:1.5, 0.5);
   }
 }
 
-function stopNote() {
-  if (currentPlay&&currentInstrument.isContinuous) {
-    currentPlay.cancel();
+function stopNote(note) {
+  if (currentPlay[note]&&currentInstrument.isContinuous) {
+    currentPlay[note].cancel();
   }
 }
 
@@ -67,37 +77,52 @@ function change(tag) {
   currentInstrument.isContinuous = continuousTable[tag];
 }
 
-canvas.onmousedown = function (event) {
-  var pos = getPos(event),
-    n = myCanvas.positionToNote(pos.x, pos.y);
-  if(n) {
+var mouseDown = function (event) {
+  var pos;
+  if(event.clientX) pos = getPos(event);
+  else if(event.touches[0]) pos = getPos(event.touches[0]);
+  noteRecord = myCanvas.positionToNote(pos.x, pos.y),
+  rectRecord = myCanvas.positionToRect(pos.x, pos.y);
+  if(noteRecord && rectRecord) {
     clickOn = true;
     mousemoveStart = 0;
-  }
-  if (pos.x && pos.y) {
-    startNote(n);
+    startNote(noteRecord);
+    myCanvas.paintKey(rectRecord, 'click');
   }
 }
+if(browser.versions.mobile)canvas.ontouchstart = mouseDown;
+else canvas.onmousedown = mouseDown;
 
-canvas.onmousemove = function(event) {
+var mouseMove = function(event) {
   if(clickOn) {
-    var pos = getPos(event);
+    var pos;
+    if(event.clientX) pos = getPos(event);
+    else if(event.touches[0]) pos = getPos(event.touches[0]);
     if(myCanvas.ifPositionChanged(pos.x, pos.y, mousemoveStart++)) {
-      stopNote(currentPlay);
-      startNote(myCanvas.positionToNote(pos.x, pos.y));
+      stopNote(noteRecord);
+      myCanvas.paintKey(rectRecord, 'release');
+      noteRecord = myCanvas.positionToNote(pos.x, pos.y),
+      rectRecord = myCanvas.positionToRect(pos.x, pos.y);
+      startNote(noteRecord);
+      myCanvas.paintKey(rectRecord, 'click');
     }
-    //if(mousemoveStart >=)
   }
 }
+if(browser.versions.mobile)canvas.ontouchmove = mouseMove;
+else canvas.onmousemove = mouseMove;
+
+var mouseUp = function() {
+  stopNote(noteRecord);
+  console.log('there');
+  myCanvas.paintKey(rectRecord, 'release');
+  clickOn = false;
+}
+if(browser.versions.mobile) document.getElementsByTagName('body')[0].ontouchend = mouseUp;
+else document.getElementsByTagName('body')[0].onmouseup = mouseUp;
 
 function getPos(e) {
   var bbox = canvas.getBoundingClientRect(),
     x = e.clientX - bbox.left * (canvas.width / bbox.width),
     y = e.clientY - bbox.top * (canvas.height / bbox.height);
     return {x : x, y : y};
-}
-
-document.getElementsByTagName('body')[0].onmouseup = function() {
-  stopNote();
-  clickOn = false;
 }
--- liveplay/mycanvas.js
+++ liveplay/mycanvas.js
@@ -19,16 +19,17 @@ function MyCanvas() {
     blackHeightRatio = 0.6,
     blackWidthRatio = 0.8,
     groupIntervalRatio = 0.05,
-    groupNum = 5,
-    keyboardTop = 0.4*h,
-    keyboardLeft = 0.15*w,
-    keyBoardBottom = 0.6*h,
-    keyBoardRight = 0.85*w,
+    groupNum = browser.versions.mobile ? 3 : 5,
+    keyboardTop = browser.versions.mobile ? 5 : 0.4*h,
+    keyboardLeft = browser.versions.mobile ? 5 : 0.15*w,
+    keyBoardBottom = browser.versions.mobile ? h-50 : 0.6*h,
+    keyBoardRight = browser.versions.mobile ? w-5 : 0.85*w,
     keyHeight = keyBoardBottom - keyboardTop,
     keyboardWidth = keyBoardRight - keyboardLeft,
     intervalWidth = keyboardWidth * groupIntervalRatio / groupNum,
     groupWidth = keyboardWidth * (1 - groupIntervalRatio) / groupNum,
     keyWidth = groupWidth / 7;
+    console.log(w);
   }
 
   this.init = function() {//窗大小变化要重调用reset()
@@ -61,19 +62,17 @@ function MyCanvas() {
       if(i == 3)continue;
       cav.moveTo(groupLeft+i*keyWidth, keyboardTop+keyHeight*blackHeightRatio);
       cav.lineTo(groupLeft+i*keyWidth, keyBoardBottom);
-      /*cav.moveTo(groupLeft+(i-blackWidthRatio/2)*keyWidth, keyboardTop);
-      cav.lineTo(groupLeft+(i-blackWidthRatio/2)*keyWidth, keyboardTop+keyHeight*blackHeightRatio);
-      cav.lineTo(groupLeft+(i+blackWidthRatio/2)*keyWidth, keyboardTop+keyHeight*blackHeightRatio);
-      cav.lineTo(groupLeft+(i+blackWidthRatio/2)*keyWidth, keyboardTop);*/
       cav.fillStyle = '#000';
       cav.fillRect(groupLeft+(i-blackWidthRatio/2)*keyWidth, keyboardTop,
         blackWidthRatio*keyWidth, keyHeight*blackHeightRatio);
     }
     cav.stroke();
 
-    var a = keyWidth < keyHeight * (1 - blackHeightRatio) ? keyWidth : keyHeight * (1 - blackHeightRatio);
-    cav.font = a + 'px Arial';
-    if(isCenC) cav.fillText('C', groupLeft+keyWidth*0.15, keyboardTop+keyHeight*0.9);
+    if(isCenC) {
+      var a = keyWidth < keyHeight * (1 - blackHeightRatio) ? keyWidth : keyHeight * (1 - blackHeightRatio);
+      cav.font = a + 'px Arial';
+      cav.fillText('C', groupLeft+keyWidth*0.15, keyboardTop+keyHeight*0.9);
+    }
   }
 
   this.positionToNote = function(x, y) {
@@ -89,15 +88,78 @@ function MyCanvas() {
       var xB = (x - groupLeftOfBlack) / keyWidth + 1,
         xBInt = parseInt(xB),
         diff = xB - xBInt;
-      if(xBInt != 3 && xBInt < 7 && xBInt > 0) {
-        if(0 < diff && diff < blackWidthRatio)
-          return notes[xBInt + '#'] + 12 * whichGroupByNote;
-      }
+      if(xBInt != 3 && xBInt < 7 && xBInt > 0 && 0 < diff && diff < blackWidthRatio)
+        return notes[xBInt + '#'] + 12 * whichGroupByNote;
     }
-
     var xW = (x - groupLeft) / keyWidth + 1,
       xWInt = parseInt(xW);
-    if(xWInt > 0 || xWInt < 8) return notes[xWInt] + whichGroupByNote * 12;
+    if(xWInt > 0 || xWInt < 8) {
+      return notes[xWInt] + whichGroupByNote * 12;
+    }
+
+    return 0;
+  }
+
+  this.positionToRect = function(x, y) {
+    if(y < keyboardTop || y > keyBoardBottom) return 0;
+    if(x < keyboardLeft || x > keyBoardRight) return 0;
+
+    var whichGroup = parseInt((x - keyboardLeft) / (groupWidth + intervalWidth)),
+      whichGroupByNote = whichGroup - parseInt(groupNum / 2),
+      groupLeft = keyboardLeft + whichGroup * (groupWidth + intervalWidth),
+      groupLeftOfBlack = groupLeft + keyWidth * (1 - blackWidthRatio / 2);
+
+    if(keyboardTop <= y && y <= keyboardTop+keyHeight*blackHeightRatio) {
+      var xB = (x - groupLeftOfBlack) / keyWidth + 1,
+        xBInt = parseInt(xB),
+        diff = xB - xBInt,
+        left = groupLeftOfBlack + (xBInt- 1) * keyWidth;
+      if(xBInt != 3 && xBInt < 7 && xBInt > 0 && 0 < diff && diff < blackWidthRatio)
+        return {
+          color : 'black',
+          clickColor : '#00cc00',
+          isCenC : false,
+          rect : [
+            {left : left,
+            top : keyboardTop,
+            width : keyWidth * blackWidthRatio,
+            height : keyHeight * blackHeightRatio
+            }
+          ]
+        };
+    };
+
+    var xW = (x - groupLeft) / keyWidth + 1,
+      xWInt = parseInt(xW),
+      left = groupLeft + (xWInt - 1) * keyWidth;
+    if(xWInt > 0 && xWInt < 8) {
+      var retValue = {
+        color : 'white',
+        clickColor : 'red',
+        isCenC : false,
+        rect : [
+          {left : left + 1,
+          top : keyboardTop + keyHeight * blackHeightRatio,
+          width : keyWidth - 2,
+          height : keyHeight * (1 - blackHeightRatio) - 1
+          },
+          {left : left + keyWidth * blackWidthRatio / 2,
+          top : keyboardTop + 1,
+          width : keyWidth * (1 - blackWidthRatio),
+          height : keyHeight * blackHeightRatio + 1
+          }
+        ]
+      };
+      if(xWInt == 1 || xWInt == 4)
+        retValue.rect[1].left = left + 1, retValue.rect[1].width = keyWidth * (1 - blackWidthRatio / 2) - 1;
+      if(xWInt == 3 || xWInt == 7)
+        retValue.rect[1].width = keyWidth * (1 - blackWidthRatio / 2) - 1;
+      if(xWInt == 1 && whichGroupByNote == 0)
+        retValue.isCenC = true;
+      return retValue;
+    }
+
+    return 0;
   }
 
   var lastPositionNote;
@@ -110,7 +172,6 @@ function MyCanvas() {
     var currentPositionNote = this.positionToNote(x, y);
     if(currentPositionNote != lastPositionNote) {
       lastPositionNote = currentPositionNote;
-      console.log(mousemoveStart);
       return true;
     }
 
@@ -118,5 +179,21 @@ function MyCanvas() {
     return false;
   }
 
-  return 0;
+  this.paintKey = function(area, cOrR) {
+    if(!area)return;
+    var color;
+    if(cOrR == 'click') color = area.clickColor;
+    else if(cOrR == 'release') color = area.color;
+    cav.fillStyle = color;
+
+    for(var i=0; i<area.rect.length; i++)
+      cav.fillRect(area.rect[i].left, area.rect[i].top, area.rect[i].width, area.rect[i].height);
+    cav.stroke();
+    if(area.isCenC) {
+      var a = keyWidth < keyHeight * (1 - blackHeightRatio) ? keyWidth : keyHeight * (1 - blackHeightRatio);
+      cav.font = a + 'px Arial';
+      cav.fillStyle = 'black';
+      cav.fillText('C', area.rect[0].left+keyWidth*0.15-1, keyboardTop+keyHeight*0.9);
+    }
+  }
 }
