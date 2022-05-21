733dd9ee796785333f3e819cccb1143547410cb8
sien75 <smooooooe@hotmail.com>
2022-05-21 15:57:22 +0800

    output webpack build information

--- script/buildUmd.js
+++ script/buildUmd.js
@@ -1,23 +1,32 @@
 const path = require('path');
 const webpack = require('webpack');
+const colors = require('colors');
 
 const configFile = path.resolve(process.argv[2]);
 const configs = require(configFile);
 
 const webpackAsync = async (config, configName) => {
     return new Promise((resolve, reject) => {
+        console.log(`building ${configName}...\n`);
         webpack(config, (err, stats) => {
-            if (err || stats.hasErrors()) {
+            if (err) {
                 console.error(err);
             } else {
-                console.log(`build ${configName} successfully!`);
+                console.log(
+                    stats.toString({
+                        colors: true,
+                    })
+                );
+                console.log(`\nbuild ${configName} successfully!`.green.bold);
             }
+            console.log('\n********************\n');
             resolve();
         });
     });
 };
 
 const taskAsync = async () => {
+    console.log('\n********************\n');
     for (const configName in configs) {
         await webpackAsync(configs[configName], configName);
     }
